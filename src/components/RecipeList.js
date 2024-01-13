aad54ab83c62bd819a99ccdc15b6d6c515d8cbc8
yahyabenjdy <100426617+yahyabenjdy@users.noreply.github.com>
2024-01-13 22:26:37 +0100

    components

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,9 +1,12 @@
 import { Link } from "react-router-dom";
+import { useTheme } from "../hooks/useTheme";
 
 // styles
 import "./RecipeList.css";
 
 export default function RecipeList({ recipes }) {
+  const { mode } = useTheme();
+
   if (recipes.length === 0) {
     return <div className="error">No recipes to load...</div>;
   }
@@ -11,10 +14,10 @@ export default function RecipeList({ recipes }) {
   return (
     <div className="recipe-list">
       {recipes.map((recipe) => (
-        <div key={recipe.id} className="card">
+        <div key={recipe.id} className={`card ${mode}`}>
           <h3>{recipe.title}</h3>
           <p>{recipe.cookingTime} to make.</p>
-          <div>{recipe.method.substring(0, 100)}</div>
+          <div>{recipe.method.substring(0, 100)}...</div>
           <Link to={`/recipes/${recipe.id}`}>Cook This</Link>
         </div>
       ))}
