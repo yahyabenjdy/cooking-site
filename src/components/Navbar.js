02418fae8efcb6a0f73dcbfa08b50919e7ced139
yahyabenjdy <100426617+yahyabenjdy@users.noreply.github.com>
2024-01-13 22:26:15 +0100

    Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom";
+import { useTheme } from "../hooks/useTheme";
 
 // styles
 import "./Navbar.css";
 import Searchbar from "./Searchbar";
 
 export default function Navbar() {
+  const { color } = useTheme();
+
   return (
-    <div className="navbar">
+    <div className="navbar" style={{ background: color }}>
       <nav>
         <Link to="/" className="brand">
           <h1>Cooking Ninja</h1>
