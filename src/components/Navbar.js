98a61cc1a8e291a77408ed4b3b559b6f16a3ed65
yahyabenjdy <100426617+yahyabenjdy@users.noreply.github.com>
2024-01-14 17:10:37 +0100

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,7 +12,7 @@ export default function Navbar() {
     <div className="navbar" style={{ background: color }}>
       <nav>
         <Link to="/" className="brand">
-          <h1>Cooking Ninja</h1>
+          <h1>Cooking</h1>
         </Link>
         <Searchbar />
         <Link to="/create">Create Recipe</Link>
