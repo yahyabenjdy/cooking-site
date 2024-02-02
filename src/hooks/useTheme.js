f165dba7c61a909710f6b3dc35239f63de93237e
Yahya Benjdy <yahyabenjdy044@gmail.com>
2024-02-02 20:12:17 +0100

    Update useTheme.js

--- src/hooks/useTheme.js
+++ src/hooks/useTheme.js
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { ThemeContext } from "../context/ThemeContext";
+import { ThemeContext } from "../context/ThemeContext"; 
 
 export const useTheme = () => {
   const context = useContext(ThemeContext);
